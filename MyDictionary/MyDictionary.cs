4d9b40bee21002ecbad70b19c41610511a0752af
Zahid Mamedov <67357750+zahid2000@users.noreply.github.com>
2021-01-23 20:58:13 +0400

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<K,T>
+    {
+        K[] index;
+        K[] indexTemp;
+        T[] value;
+        T[] valueTemp;
+
+        public MyDictionary()
+        {
+            index = new K[0];
+            value = new T[0];
+        }
+
+        public void Add(K _index,T _value)
+        {
+            indexTemp = index;
+
+            index = new K[index.Length + 1];
+            for (int i = 0; i < indexTemp.Length; i++)
+            {
+                index[i] = indexTemp[i];
+            }
+            index[index.Length - 1] = _index;
+
+            valueTemp = value;
+            value = new T[value.Length + 1];
+
+            for (int i = 0; i < valueTemp.Length; i++)
+            {
+                value[i] = valueTemp[i];
+            }
+            value[value.Length - 1] = _value;
+        }
+
+        public void View()
+        {
+            for (int i = 0; i < index.Length; i++)
+            {
+                Console.WriteLine("Index : " + index[i] +"  " + "Value : "+value[i]+"\n");
+            }
+            ;
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Yusif");
+            myDictionary.Add(2, "Zahid");
+            myDictionary.Add(3, "Inci");
+
+            myDictionary.View();
+        }
+    }
+}
